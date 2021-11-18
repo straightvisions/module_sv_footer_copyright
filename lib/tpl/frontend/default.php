7ac41e3b3621099819d50fd527921b5691c73712
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-11-18 18:17:15 +0100

    CHECK FOR SIDEBAR OCCURANCE

--- lib/tpl/frontend/default.php
+++ lib/tpl/frontend/default.php
@@ -1,6 +1,10 @@
 <div class="<?php echo $this->get_prefix('wrapper'); ?>">
 	<div class="<?php echo $this->get_prefix('inner'); ?>">
+		<?php if($this->get_module( 'sv_sidebar' )->load( $this->get_setting('sidebar_1')->get_data() )){ ?>
 		<div class="sv100_sv_sidebar_sv_copyright_footer_1"><?php echo $this->get_module( 'sv_sidebar' )->load( $this->get_setting('sidebar_1')->get_data() ); ?></div>
+		<?php } ?>
+		<?php if($this->get_module( 'sv_sidebar' )->load( $this->get_setting('sidebar_2')->get_data() )){ ?>
 		<div class="sv100_sv_sidebar_sv_copyright_footer_2"><?php echo $this->get_module( 'sv_sidebar' )->load( $this->get_setting('sidebar_2')->get_data() ); ?></div>
+		<?php } ?>
 	</div>
 </div>
