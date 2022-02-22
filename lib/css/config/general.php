86f6cfad5600bd8749fcd7597f8dc93e049c54e5
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-02-22 17:19:28 +0100

    ###PREPARE FOR FOOTER BLOCKS### REMOVE FOOTER COMMON LINK CSS OUTPUT

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -21,22 +21,3 @@
 			$module->get_setting('padding')->get_css_data('padding')
 		)
 	);
-
-	// links ----------------------------------------------------------------------------------------------------
-	echo $_s->build_css(
-		'.sv100_sv_footer_copyright_wrapper .sv100_sv_footer_copyright_inner a',
-		array_merge(
-			$module->get_setting('text_color_link')->get_css_data(),
-			$module->get_setting('text_bg_color_link')->get_css_data('background-color'),
-			$module->get_setting('text_deco_link')->get_css_data('text-decoration')
-		)
-	);
-
-	echo $_s->build_css(
-		'.sv100_sv_footer_copyright_wrapper .sv100_sv_footer_copyright_inner a:hover',
-		array_merge(
-			$module->get_setting('text_color_link_hover')->get_css_data(),
-			$module->get_setting('text_bg_color_link_hover')->get_css_data('background-color'),
-			$module->get_setting('text_deco_link_hover')->get_css_data('text-decoration')
-		)
-	);
