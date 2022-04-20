3863ad12f83a208105568462d71ac1091b6ba4ce
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-04-20 15:11:06 +0200

    Feat - Add - Override Sidebar via Metabox in Post

--- lib/tpl/frontend/default.php
+++ lib/tpl/frontend/default.php
@@ -1,5 +1,5 @@
 <div class="<?php echo $this->get_prefix('wrapper'); ?>">
 	<div class="<?php echo $this->get_prefix('inner'); ?>">
-		<div class="sv100_sv_sidebar_sv_copyright_footer"><?php echo $this->get_module( 'sv_sidebar' )->load( $this->get_setting('sidebar')->get_data() ); ?></div>
+		<div class="sv100_sv_sidebar_sv_copyright_footer"><?php echo $this->get_module( 'sv_sidebar' )->load( $this->show_sidebar() ); ?></div>
 	</div>
 </div>
--- sv_footer_copyright.php
+++ sv_footer_copyright.php
@@ -11,9 +11,12 @@
 				->set_section_template_path()
 				->set_section_order(4100)
 				->set_section_icon('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 15.781c-2.084 0-3.781-1.696-3.781-3.781s1.696-3.781 3.781-3.781c1.172 0 2.306.523 3.136 1.669l1.857-1.218c-1.281-1.826-3.133-2.67-4.993-2.67-3.308 0-6 2.692-6 6s2.691 6 6 6c1.881 0 3.724-.859 4.994-2.67l-1.857-1.218c-.828 1.14-1.959 1.669-3.137 1.669z"/></svg>')
-				->load_settings()
 				->get_root()
 				->add_section( $this );
+
+			add_action('init', function(){
+				$this->load_settings()->add_metaboxes();
+			});
 		}
 		protected function load_settings(): sv_footer_copyright {
 			// General
@@ -40,12 +43,12 @@
 			return $this;
 		}
 
-		public function has_footer_content(): bool {
+		public function has_sidebar_content(): bool {
 			if ( !$this->get_module( 'sv_sidebar' ) ) {
 				return false;
 			}
 
-			if( $this->get_module( 'sv_sidebar' )->load( $this->get_setting('sidebar')->get_data() ) ) {
+			if( $this->get_module( 'sv_sidebar' )->load( $this->get_metabox_data('sidebar') ) ) {
 				return true;
 			}
 
@@ -57,7 +60,7 @@
 			if(!is_admin()){
 				$this->load_settings();
 
-				if ( $this->has_footer_content() ) {
+				if ( $this->has_sidebar_content() ) {
 					$this->register_scripts();
 
 					ob_start();
@@ -72,5 +75,21 @@
 				}
 			}
 			return $output;
+		}
+		private function add_metaboxes(): sv_footer_COPYRIGHT{
+			$this->metaboxes			= $this->get_root()->get_module('sv_metabox');
+
+			$states						= $this->get_module('sv_sidebar') ? $this->get_module('sv_sidebar')->get_sidebars_for_metabox_options() : array('' => __('Please activate module SV Sidebar for this Feature.', 'sv100'));
+
+			$this->metaboxes->get_setting( $this->get_prefix('sidebar') )
+				->set_title( __('Sidebar Footer Copyright', 'sv100') )
+				->set_description( __('Override Default Settings', 'sv100') )
+				->load_type( 'select' )
+				->set_options($states);
+
+			return $this;
+		}
+		public function show_sidebar(): string{
+			return $this->has_sidebar_content() ? $this->get_metabox_data('sidebar') : '';
 		}
 	}
